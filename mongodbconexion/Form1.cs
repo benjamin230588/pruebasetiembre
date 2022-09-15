40b30b7350b5511e38b352b2c23eba9597a60f0f
benjamin230588 <55041841+benjamin230588@users.noreply.github.com>
2022-09-14 21:05:10 -0500

    Agregar archivos de proyecto.

---
+++ mongodbconexion/Form1.cs
@@ -0,0 +1,34 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mongodbconexion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            MongoClient cliente = new MongoClient("mongodb://127.0.0.1:27017");
+            var database = cliente.GetDatabase("prueba");
+            var dbc = database.GetCollection<producto>("producto");
+
+            var objeto = new producto() { name = "Josue", edad = 34 };
+            dbc.InsertOne(objeto);
+            var lista = dbc.Find(d => true).ToList();
+         }
+    }
+}
---
+++ mongodbconexion/producto.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mongodbconexion
+{
+    public class producto
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string id { get; set; }
+        [BsonElement("name")]
+        public string name { get; set; }
+        [BsonElement("edad")]
+        public int edad { get; set; }
+
+    }
+}
